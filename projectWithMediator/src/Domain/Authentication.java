830555a51494f2571b46648a1108c5d5e4c0d914
Buse Öznur Özkan <99618976+buseoznurozkan@users.noreply.github.com>
2023-11-26 16:12:56 +0300

    Delete projectWithMediator directory

--- projectWithMediator/src/Domain/Authentication.java
+++
@@ -1,8 +0,0 @@
-package Domain;
-
-public class Authentication {
-
-    //Authentication		login(String, String)
-    //validationResponse()
-
-}
--- projectWithMediator/src/Domain/Cards.java
+++
@@ -1,13 +0,0 @@
-package Domain;
-
-public abstract class Cards{
-
-    //<<abstract class>> Cards	cardname cardeffect
-
-    private String cardName;
-
-    public Cards(String cardName) {
-        this.cardName = cardName;
-    }
-
-}
