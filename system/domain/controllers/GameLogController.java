b4f62eefaaeeb681d3fa628a66fdda16b32a3022
buseoznurozkan <bozkan19@ku.edu.tr>
2023-11-30 23:26:19 +0300

    gamelogcontroller debugged

--- system/domain/controllers/GameLogController.java
+++ system/domain/controllers/GameLogController.java
@@ -14,18 +14,20 @@ public class GameLogController {
     private Map<Player, List<GameAction>> gameActions;
     
     public GameLogController GameLogController(Player player1, Player player2){
-        gameAction startAction1 = new gameAction("Game", player1.getName(), "GameLog Start", 0);
-        gameAction startAction2 = new gameAction("Game", player2.getName(), "GameLog Start", 0);
+        GameAction startAction1 = new GameAction("Game", player1.getName(), "GameLog Start", 0);
+        GameAction startAction2 = new GameAction("Game", player2.getName(), "GameLog Start", 0);
 
-        gameActions.put(player1, startAction1);
-        gameActions.put(player1, startAction2);
+
+        gameActions.put(player1, List.of(startAction1));
+        gameActions.put(player2, List.of(startAction2));
+        return this;
 
     }
 
     public void showGameLog(Player player) {
-        List<E> tail = gameActions.get(player).subList(Math.max(l.size() - 3, 0), l.size()); //gets the players list and then takes the last 3 elements as tail.
+        List<GameAction> tail = gameActions.get(player).subList(Math.max(l.size() - 3, 0), l.size()); //gets the players list and then takes the last 3 elements as tail.
         
-        for (i = 0; i < tail.size(); i++) 
+        for (int i = 0; i < tail.size(); i++) 
         {
             tail.get(i).toString(); //iterates over the elements and prints by using string method.
         }
