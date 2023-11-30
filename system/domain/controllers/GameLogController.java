b9504276093324313853ea6892dade28b6c911ab
Merken19 <Merken19@ku.edu.tr>
2023-11-30 21:57:31 +0300

    game log controller initalize

--- system/domain/controllers/GameLogController.java
+++ system/domain/controllers/GameLogController.java
@@ -13,6 +13,15 @@ public class GameLogController {
 
     private Map<Player, List<GameAction>> gameActions;
     
+    public GameLogController GameLogController(Player player1, Player player2){
+        gameAction startAction1 = new gameAction("Game", player1.getName(), "Game Start", 0);
+        gameAction startAction2 = new gameAction("Game", player2.getName(), "Game Start", 0);
+
+        gameActions.put(player1, startAction1);
+        gameActions.put(player1, startAction2);
+
+    }
+
     public void showGameLog(Player player) {
         List<E> tail = gameActions.get(player).subList(Math.max(l.size() - 3, 0), l.size()); //gets the players list and then takes the last 3 elements as tail.
         
