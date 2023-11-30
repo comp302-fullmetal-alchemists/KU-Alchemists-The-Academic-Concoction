399accbee54cea77c5dd49044dc5004514ef88a3
Merken19 <Merken19@ku.edu.tr>
2023-11-30 21:45:26 +0300

    gameLog prints, commented files

--- system/domain/GameAction.java
+++ system/domain/GameAction.java
@@ -8,12 +8,13 @@ public class GameAction {
     private int score;
 
     public GameAction (String from, String to, String action, int score) {
-        this.gameAction = gameAction;
+        this.from = from;
+        this.to = to;
+        this.action = action;
         this.score = score;
     }
     
-    //TODO
-    public String toString() {
-        return "";
+    public String toString() { //Example "From player1 to player2, 3 gold given." or "From Player1 to Adventurer, negative potion given."
+        return String.format("From %s to %s, %s\n", from, to, action);
     }
 }
