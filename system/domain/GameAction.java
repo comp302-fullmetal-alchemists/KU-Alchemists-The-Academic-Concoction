0d3e41bffd056ca52fd26ef85d26ad69d8f5e873
Merken19 <Merken19@ku.edu.tr>
2023-11-30 19:42:02 +0300

    todo tostring

--- system/domain/GameAction.java
+++ system/domain/GameAction.java
@@ -2,11 +2,17 @@ package system.domain;
 
 public class GameAction {
 
-    //GameAction	from:
-    //to:
-    //action:
-    //score:	toString()
+    private String from;
+    private String to;
+    private String action;
+    private int score;
 
+    public GameAction (String from, String to, String action, int score) {
+        this.gameAction = gameAction;
+        this.score = score;
+    }
+    
+    //TODO
     public String toString() {
         return "";
     }
