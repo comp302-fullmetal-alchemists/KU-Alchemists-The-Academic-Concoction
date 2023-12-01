b559ccdb454ac13133dea0607d12462b4b298b2f
mislinaakca <makca21@ku.edu.tr>
2023-12-01 17:14:16 +0300

    pause/resume game demo handled

--- system/ui/frame/Gameboard.java
+++ system/ui/frame/Gameboard.java
@@ -19,7 +19,7 @@ public class Gameboard extends JFrame{
     
     public Gameboard() {
         super();
-        this.gameController = new GameBoardController();
+        this.gameController = GameBoardController.getInstance();
 
         this.authPanel = new AuthenticationPanel(this);
         add(authPanel);
@@ -27,7 +27,7 @@ public class Gameboard extends JFrame{
         setSize(600, 600);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        validate(); 
+        validate();
     }
 
     public void initializeTheBoard() {
