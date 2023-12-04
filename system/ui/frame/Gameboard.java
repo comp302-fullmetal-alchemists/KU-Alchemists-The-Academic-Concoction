baf17d7046c09d83dd06f3a7caa2c32279d4a1be
mislinaakca <makca21@ku.edu.tr>
2023-12-05 01:46:44 +0300

    login page UI implemented

--- system/ui/frame/Gameboard.java
+++ system/ui/frame/Gameboard.java
@@ -1,60 +1,81 @@
 package system.ui.frame;
 
-import system.domain.Player;
-import system.domain.controllers.GameBoardController;
-import system.domain.interfaces.Observer;
-import system.ui.interfaces.*;
-import system.ui.panels.AuthenticationPanel;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
 
 import javax.swing.JFrame;
+import system.domain.controllers.GameBoardController;
+import system.domain.interfaces.Observer;
+import system.ui.interfaces.Mediator;
+import system.ui.interfaces.PlayerMediator;
+import system.ui.panels.AuthenticationPanel;
 
-import java.awt.GridLayout;
 
 public class Gameboard extends JFrame implements Observer{
 
-    AuthenticationPanel authPanel;
+	private static final long serialVersionUID = 1L;
+	AuthenticationPanel authPanel;
     PlayerContentPane playerPane;
     GameContentPane gamePane;
     PlayerMediator mediator;
     GameBoardController gameController;
-    
-    public Gameboard() {
-        super();
-        this.gameController = GameBoardController.getInstance();
-        gameController.setObserver(this);
-        this.authPanel = new AuthenticationPanel();
-        add(authPanel);
-        this.mediator = new Mediator();
-        setSize(600, 600);
-        setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        validate();
-    }
-    
-    @Override
-    public void update(String msg) {
-        if (msg.equals("INITIALIZE_BOARD")) {
+  
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Gameboard frame = new Gameboard();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+	public Gameboard() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(70, 50, 650, 400);
+		this.gameController = GameBoardController.getInstance();
+		gameController.setObserver(this);
+		this.authPanel = new AuthenticationPanel() ;
+		authPanel.setBounds(600, 153, 0, 0);
+		getContentPane().add(authPanel);
+		authPanel.setLayout(null);
+		this.mediator = new Mediator();
+		setSize(859, 607);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		validate();
+		
+		
+	}
+
+	@Override
+	public void update(String msg) {
+		// TODO Auto-generated method stub
+		if (msg.equals("INITIALIZE_BOARD")) {
             initializeTheBoard();
         }
         else if (msg.equals("CHANGE_PLAYER")) {
             changePlayer();
         }
-    }
-
-    public void changePlayer() {
+	}
+	
+	public void changePlayer() { 
         playerPane.changeView(gameController.getCurrentPlayer().getName());
     }
 
-    public void initializeTheBoard() {
+    public void initializeTheBoard() { 
         remove(authPanel);
-        setLayout(new GridLayout(1, 2));
+        getContentPane().setLayout(new GridLayout(1, 2));
         this.playerPane = new PlayerContentPane(gameController.getPlayer(0), gameController.getPlayer(1), mediator);
         this.gamePane = new GameContentPane(mediator);
         playerPane.changeView(gameController.getCurrentPlayer().getName());
-        add(gamePane);
-        add(playerPane);
+        getContentPane().add(gamePane);
+        getContentPane().add(playerPane);
         revalidate();
     }
-
 
 }
