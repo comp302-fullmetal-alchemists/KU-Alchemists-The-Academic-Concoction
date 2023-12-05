bb917e8bd168ef364248e9f166f04a85ac25752e
durutandogan <91738899+durutandogan@users.noreply.github.com>
2023-12-05 21:02:30 +0300

    ingredient storage UI & color changed

--- system/ui/panels/AuthenticationPanel.java
+++ system/ui/panels/AuthenticationPanel.java
@@ -65,6 +65,74 @@ public class AuthenticationPanel extends JPanel implements Observer{
 
 
 }*/
+/*package system.ui.panels;
+
+import system.domain.controllers.AuthenticationController;
+import system.domain.Player;
+import system.domain.interfaces.Observer;
+
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class AuthenticationPanel extends JPanel implements Observer{
+    private JButton loginButton;
+    private JTextField usernameField1;
+    private JTextField usernameField2;
+    private JTextField tokenField1;
+    private JTextField tokenField2;
+    private JLabel usernameLabel;
+    private JLabel tokenLabel;
+    private AuthenticationController authController;
+
+    public AuthenticationPanel(){
+        super();
+        this.authController = new AuthenticationController();
+        authController.setObserver(this);
+        this.usernameLabel = new JLabel("Usernames");
+        add(usernameLabel);
+        this.usernameField1 = new JTextField(23);
+        usernameField1.setText("player1");
+        add(usernameField1);
+        this.usernameField2 = new JTextField(23);
+        usernameField2.setText("player2");
+        add(usernameField2);
+        this.tokenLabel = new JLabel("Tokens");
+        add(tokenLabel);
+        this.tokenField1 = new JTextField(10);
+        add(tokenField1);
+        tokenField1.setText("token1");
+        this.tokenField2 = new JTextField(10);
+        add(tokenField2);
+        tokenField2.setText("token2");
+        this.loginButton = new JButton("Login");
+        add(loginButton);
+        
+        loginButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                String username1 = usernameField1.getText();
+                String username2 = usernameField2.getText();
+                String token1 = new String(tokenField1.getText());
+                String token2 = new String(tokenField2.getText());
+                authController.login(username1, token1, username2, token2);
+            }
+        });
+    }
+
+    @Override
+    public void update(String msg) {
+        if (!msg.equals("VALID")) {
+            JOptionPane.showMessageDialog(this, msg);
+        }
+    }
+
+
+}*/
 package system.ui.panels;
 
 import javax.swing.JPanel;
@@ -95,7 +163,7 @@ public class AuthenticationPanel extends JPanel implements Observer {
 	 */
 	public AuthenticationPanel() {
 	
-		setBackground(new Color(51, 102, 153));
+		setBackground(new Color(58, 77, 108));
 		setLayout(null);
 		
 		this.authController = new AuthenticationController();
@@ -104,7 +172,7 @@ public class AuthenticationPanel extends JPanel implements Observer {
 		JTextArea txtWelcome = new JTextArea();
 		txtWelcome.setForeground(new Color(255, 255, 255));
 		txtWelcome.setFont(new Font("Microsoft Sans Serif", Font.BOLD | Font.ITALIC, 30));
-		txtWelcome.setBackground(new Color(51, 102, 153));
+		txtWelcome.setBackground(new Color(58, 77, 108));
 		txtWelcome.setText("WELCOME TO KUALCHEMISTS!");
 		txtWelcome.setBounds(190, 29, 697, 87);
         txtWelcome.setEditable(false);
@@ -114,7 +182,7 @@ public class AuthenticationPanel extends JPanel implements Observer {
 		txtInformMessage.setText("Please enter usernames and choose tokens to start the game!");
 		txtInformMessage.setForeground(Color.WHITE);
 		txtInformMessage.setFont(new Font("Microsoft Sans Serif", Font.BOLD | Font.ITALIC, 23));
-		txtInformMessage.setBackground(new Color(51, 102, 153));
+		txtInformMessage.setBackground(new Color(58, 77, 108));
 		txtInformMessage.setBounds(90, 140, 697, 46);
         txtInformMessage.setEditable(false);
 		add(txtInformMessage);
@@ -123,7 +191,7 @@ public class AuthenticationPanel extends JPanel implements Observer {
 		txtrPlayer1Username.setText("Player 1 username:\n");
 		txtrPlayer1Username.setForeground(Color.WHITE);
 		txtrPlayer1Username.setFont(new Font("Microsoft Sans Serif", Font.BOLD | Font.ITALIC, 14));
-		txtrPlayer1Username.setBackground(new Color(51, 102, 153));
+		txtrPlayer1Username.setBackground(new Color(58, 77, 108));
 		txtrPlayer1Username.setBounds(36, 214, 166, 32);
         txtrPlayer1Username.setEditable(false);
 		add(txtrPlayer1Username);
@@ -132,7 +200,7 @@ public class AuthenticationPanel extends JPanel implements Observer {
 		txtrPlayer2Username.setText("Player 2 username:\n");
 		txtrPlayer2Username.setForeground(Color.WHITE);
 		txtrPlayer2Username.setFont(new Font("Microsoft Sans Serif", Font.BOLD | Font.ITALIC, 14));
-		txtrPlayer2Username.setBackground(new Color(51, 102, 153));
+		txtrPlayer2Username.setBackground(new Color(58, 77, 108));
 		txtrPlayer2Username.setBounds(36, 324, 141, 32);
         txtrPlayer2Username.setEditable(false);
 		add(txtrPlayer2Username);
@@ -141,7 +209,7 @@ public class AuthenticationPanel extends JPanel implements Observer {
 		txtrPlayer1TokenChoose.setText("Player 1 choose a token:\n");
 		txtrPlayer1TokenChoose.setForeground(Color.WHITE);
 		txtrPlayer1TokenChoose.setFont(new Font("Microsoft Sans Serif", Font.BOLD | Font.ITALIC, 14));
-		txtrPlayer1TokenChoose.setBackground(new Color(51, 102, 153));
+		txtrPlayer1TokenChoose.setBackground(new Color(58, 77, 108));
 		txtrPlayer1TokenChoose.setBounds(442, 214, 166, 32);
         txtrPlayer1TokenChoose.setEditable(false);
 		add(txtrPlayer1TokenChoose);
@@ -150,7 +218,7 @@ public class AuthenticationPanel extends JPanel implements Observer {
 		txtrPlayer2TokenChoose.setText("Player 2 choose a token:\n");
 		txtrPlayer2TokenChoose.setForeground(Color.WHITE);
 		txtrPlayer2TokenChoose.setFont(new Font("Microsoft Sans Serif", Font.BOLD | Font.ITALIC, 14));
-		txtrPlayer2TokenChoose.setBackground(new Color(51, 102, 153));
+		txtrPlayer2TokenChoose.setBackground(new Color(58, 77, 108));
 		txtrPlayer2TokenChoose.setBounds(442, 324, 166, 32);
         txtrPlayer2TokenChoose.setEditable(false);
 		add(txtrPlayer2TokenChoose);
@@ -197,7 +265,7 @@ public class AuthenticationPanel extends JPanel implements Observer {
 		txtrAGameBy.setText("A game by FullMetal Alchemists\n");
 		txtrAGameBy.setForeground(Color.WHITE);
 		txtrAGameBy.setFont(new Font("Luminari", Font.ITALIC, 14));
-		txtrAGameBy.setBackground(new Color(51, 102, 153));
+		txtrAGameBy.setBackground(new Color(58, 77, 108));
 		txtrAGameBy.setBounds(600, 532, 241, 32);
         txtrAGameBy.setEditable(false);
 		add(txtrAGameBy);
