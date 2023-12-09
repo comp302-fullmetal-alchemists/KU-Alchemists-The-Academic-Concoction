af78e5b3b331e7de4ff54a4cf130e7995e685dca
NeslisahCebeci <ncebeci19@ku.edu.tr>
2023-12-09 21:30:33 +0300

    Authentication UI is organized

--- system/ui/panels/AuthenticationPanel.java
+++ system/ui/panels/AuthenticationPanel.java
@@ -47,7 +47,7 @@ public class AuthenticationPanel extends JPanel implements Observer {
 		txtWelcome.setFont(new Font("Monotype Corsiva", Font.BOLD, 60));
 		txtWelcome.setBackground(new Color(58, 77, 108));
 		txtWelcome.setText("WELCOME TO KUALCHEMISTS!");
-		txtWelcome.setBounds(304, 37, 920, 82);
+		txtWelcome.setBounds(152, 38, 908, 72);
 		txtWelcome.setEditable(false);
 
 		add(txtWelcome);
@@ -57,18 +57,18 @@ public class AuthenticationPanel extends JPanel implements Observer {
 		txtInformMessage.setForeground(Color.WHITE);
 		txtInformMessage.setFont(new Font("Microsoft Sans Serif", Font.BOLD | Font.ITALIC, 25));
 		txtInformMessage.setBackground(new Color(58, 77, 108));
-		txtInformMessage.setBounds(356, 129, 975, 46);
+		txtInformMessage.setBounds(213, 120, 975, 46);
 		txtInformMessage.setEditable(false);
 		add(txtInformMessage);
 
-    JTextArea txtrPlayer1Username = new JTextArea();
+    	JTextArea txtrPlayer1Username = new JTextArea();
 		txtrPlayer1Username.setText("Player 1 username:\n");
 		txtrPlayer1Username.setForeground(Color.WHITE);
 		txtrPlayer1Username.setFont(new Font("Microsoft Sans Serif", Font.BOLD | Font.ITALIC, 20));
 		txtrPlayer1Username.setBackground(new Color(58, 77, 108));
 		/*txtrPlayer1Username.setBounds(270, 212, 212, 32);*/
-		txtrPlayer1Username.setBounds(267, 214, 166, 32);
-    txtrPlayer1Username.setEditable(false);
+		txtrPlayer1Username.setBounds(152, 176, 212, 32);
+    	txtrPlayer1Username.setEditable(false);
 		add(txtrPlayer1Username);
 
 		JTextArea txtrPlayer2Username = new JTextArea();
@@ -77,8 +77,8 @@ public class AuthenticationPanel extends JPanel implements Observer {
 		txtrPlayer2Username.setFont(new Font("Microsoft Sans Serif", Font.BOLD | Font.ITALIC, 20));
 		txtrPlayer2Username.setBackground(new Color(58, 77, 108));
 		/*txtrPlayer2Username.setBounds(905, 212, 141, 32);*/
-		txtrPlayer2Username.setBounds(267, 337, 141, 32);
-    txtrPlayer2Username.setEditable(false);
+		txtrPlayer2Username.setBounds(687, 177, 221, 32);
+   		txtrPlayer2Username.setEditable(false);
 		add(txtrPlayer2Username);
 
         JTextArea txtrPlayer1TokenChoose = new JTextArea();
@@ -87,8 +87,8 @@ public class AuthenticationPanel extends JPanel implements Observer {
 		txtrPlayer1TokenChoose.setFont(new Font("Microsoft Sans Serif", Font.BOLD | Font.ITALIC, 20));
 		txtrPlayer1TokenChoose.setBackground(new Color(58, 77, 108));
 		/*txtrPlayer1TokenChoose.setBounds(258, 397, 332, 32);*/
-		txtrPlayer1TokenChoose.setBounds(815, 214, 166, 32);
-    txtrPlayer1TokenChoose.setEditable(false);
+		txtrPlayer1TokenChoose.setBounds(152, 307, 332, 32);
+    	txtrPlayer1TokenChoose.setEditable(false);
 		add(txtrPlayer1TokenChoose);
 
 		JTextArea txtrPlayer2TokenChoose = new JTextArea();
@@ -97,50 +97,50 @@ public class AuthenticationPanel extends JPanel implements Observer {
 		txtrPlayer2TokenChoose.setFont(new Font("Microsoft Sans Serif", Font.BOLD | Font.ITALIC, 20));
 		txtrPlayer2TokenChoose.setBackground(new Color(58, 77, 108));
 		/*txtrPlayer2TokenChoose.setBounds(880, 397, 296, 32);*/
-    txtrPlayer2TokenChoose.setBounds(815, 337, 166, 32);
-    txtrPlayer2TokenChoose.setEditable(false);
+    	txtrPlayer2TokenChoose.setBounds(687, 307, 296, 32);
+    	txtrPlayer2TokenChoose.setEditable(false);
 		add(txtrPlayer2TokenChoose);
 
-    username1 = new JTextField();
-    username1.setFont(new Font("Tahoma", Font.PLAIN, 21));
-    username1.setBounds(286, 242, 166, 53);
-    /*username1.setBounds(270, 276, 166, 53);*/
-    add(username1);
-    username1.setColumns(10);
+		username1 = new JTextField();
+		username1.setFont(new Font("Tahoma", Font.PLAIN, 21));
+		username1.setBounds(157, 229, 166, 53);
+		/*username1.setBounds(270, 276, 166, 53);*/
+		add(username1);
+		username1.setColumns(10);
 
-    username2 = new JTextField();
-    username2.setFont(new Font("Tahoma", Font.PLAIN, 21));
-    username2.setBounds(286, 368, 166, 53);
-    /*username2.setBounds(905, 276, 166, 53);*/
-    username2.setColumns(10);
-    add(username2);
+		username2 = new JTextField();
+		username2.setFont(new Font("Tahoma", Font.PLAIN, 21));
+		username2.setBounds(691, 229, 166, 53);
+		/*username2.setBounds(905, 276, 166, 53);*/
+		username2.setColumns(10);
+		add(username2);
 
-    token1 = new JComboBox();
-    token1.setBounds(246, 439, 348, 268);
-    token1.setBackground(new Color(58, 77, 108));
-    token1.setModel(loadImages());
-    token1.addActionListener(new ActionListener() {
+		token1 = new JComboBox();
+		token1.setBounds(152, 357, 348, 268);
+		token1.setBackground(new Color(58, 77, 108));
+		token1.setModel(loadImages());
+    	token1.addActionListener(new ActionListener() {
         public void actionPerformed(ActionEvent e) {
             token1.getSelectedItem();
         }
-    });
-    add(token1);
-    token1.setVisible(true);
+    	});
+		add(token1);
+		token1.setVisible(true);
 
-    token2 = new JComboBox();
-    token2.setBounds(873, 439, 351, 268);
-    token2.setBackground(new Color(58, 77, 108));
-    token2.setModel(loadImages());
-    token2.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-            token2.getSelectedItem();
-        }
-    });
-    add(token2);
-    token2.setVisible(true);
+		token2 = new JComboBox();
+		token2.setBounds(687, 357, 351, 268);
+		token2.setBackground(new Color(58, 77, 108));
+		token2.setModel(loadImages());
+		token2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				token2.getSelectedItem();
+			}
+		});
+		add(token2);
+		token2.setVisible(true);
 
-    JButton loginButton = new JButton("LOGIN");
-    loginButton.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		JButton loginButton = new JButton("LOGIN");
+		loginButton.setFont(new Font("Tahoma", Font.PLAIN, 25));
 
 		loginButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -152,18 +152,18 @@ public class AuthenticationPanel extends JPanel implements Observer {
 	            
 			}
 		});
-		/*loginButton.setBounds(1105, 747, 166, 46);*/
-		loginButton.setBounds(889, 527, 166, 46);
+		/*loginButton.setBounds(1105, 747, 166, 46); */
+		loginButton.setBounds(978, 650, 166, 42);
 		add(loginButton);
 		
 		JTextArea txtrAGameBy = new JTextArea();
 		txtrAGameBy.setText("A game by FullMetal Alchemists\n");
 		txtrAGameBy.setForeground(Color.WHITE);
-		txtrAGameBy.setFont(new Font("Luminari", Font.ITALIC, 25));
+		txtrAGameBy.setFont(new Font("Luminari", Font.ITALIC, 18));
 		txtrAGameBy.setBackground(new Color(58, 77, 108));
 		/*txtrAGameBy.setBounds(1004, 803, 468, 32);*/
-		txtrAGameBy.setBounds(938, 710, 241, 32);
-    txtrAGameBy.setEditable(false);
+		txtrAGameBy.setBounds(880, 695, 300, 32);
+    	txtrAGameBy.setEditable(false);
 		add(txtrAGameBy);
 	}
 
