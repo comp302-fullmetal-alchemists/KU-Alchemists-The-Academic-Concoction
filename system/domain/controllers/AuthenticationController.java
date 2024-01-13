325d1583a6e91293e88a4597a23e172a15fd35b6
odiler <odiler19@ku.edu.tr>
2024-01-14 00:40:41 +0300

    authentication revised for network

--- system/domain/controllers/AuthenticationController.java
+++ system/domain/controllers/AuthenticationController.java
@@ -9,11 +9,17 @@ import java.util.ArrayList;
 import system.domain.interfaces.Observer;
 
 public class AuthenticationController {
- 
+    
+    private static AuthenticationController instance;
     private Observer authenticationUI;
+    private String tempUsername;
+    private Icon tempToken;
 
-    public AuthenticationController() {
-
+    public static AuthenticationController getInstance(){
+        if (instance == null) {
+            instance = new AuthenticationController();
+        }
+        return instance;
     }
 
     // This method is called by the UI when the user clicks the login button
@@ -21,26 +27,6 @@ public class AuthenticationController {
         this.authenticationUI = observer;
     }
     
-    /* 
-    // This method is called by the UI when the user clicks the login button
-    public void login(String username1, Icon token1, String username2, Icon token2) {
-        //chechks if the usernames are the same
-        if (username1.equals(username2)){
-            authenticationUI.update("Usernames cannot be the same");
-        }
-        //checks whether usernames are empty
-        else if (username1.equals("") || username2.equals("")){
-            authenticationUI.update("Please fill all the fields");
-        }
-        //creates 2 players and calls the initializeTheBoard method
-        else{
-        	ArrayList<Player> players = new ArrayList<Player>();
-        	players.add(new Player(username1, token1));
-        	players.add(new Player(username2, token2));
-            GameBoardController.getInstance().initializeTheBoard(players.get(0), players.get(1));
-            authenticationUI.update("VALID");
-    }
-    */
 
     // This method is called by the UI when the user clicks the login button
     public void login(String username, Icon token){
@@ -48,16 +34,22 @@ public class AuthenticationController {
         if (username.equals("")){
             authenticationUI.update("Please fill all the fields");
         }
-
-        else if (GameBoardController.getInstance().getClientAdapter().validateUserChoices(username)){
-            Player player = new Player(username, token);
-            GameBoardController.getInstance().getClientAdapter().registerPlayer(player);
-            authenticationUI.update("VALID");
-        }
-        
-        else{
-            authenticationUI.update("Username already exists");
+        else {
+            this.tempUsername = username;
+            this.tempToken = token;
+            GameBoardController.getInstance().getClientAdapter().validateUserChoices(username);
         }
     }
+
+    public void invalidUsername() {
+        authenticationUI.update("Username already exists");
+    }
+
+    public void validUsername() {
+        Player player = new Player(tempUsername, tempToken);
+        GameBoardController.getInstance().getClientAdapter().registerPlayer(player);
+        authenticationUI.update("VALID");
+    }
+    
 }
 
