c28467b10b4867a9b0e51cf2f5d8ce45ae2c3bb0
durutandogan <91738899+durutandogan@users.noreply.github.com>
2024-01-17 19:42:55 +0300

    fixed sizing issue

--- system/ui/panels/WelcomePagePanel.java
+++ system/ui/panels/WelcomePagePanel.java
@@ -16,7 +16,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Font;
 
-public class WelcomePagePanel extends JPanel { 
+public class WelcomePagePanel extends JPanel { //class ismi değiştir
  
     JButton startGameButton;
     JComboBox<Integer> numberOfPlayers;
@@ -26,20 +26,21 @@ public class WelcomePagePanel extends JPanel {
 
 
     public WelcomePagePanel(Gameboard gameboard) {
-    	setBackground(new Color(58, 77, 108)); 
+    	setSize(1200, 800);
+    	setBackground(new Color(58, 77, 108)); //isimleri değiştirdim tek
         this.controller = GameBoardController.getInstance().getWelcomeController();
         setLayout(null);
         // Offline Section
         JLabel offlineLabel = new JLabel("OFFLINE GAME MODE");
+        offlineLabel.setBounds(64, 181, 358, 39);
         offlineLabel.setForeground(new Color(255, 255, 255));
-        offlineLabel.setFont(new Font("Telugu MN", Font.BOLD | Font.ITALIC, 20));
-        offlineLabel.setBounds(23, 173, 283, 39);
+        offlineLabel.setFont(new Font("Telugu MN", Font.BOLD | Font.ITALIC, 30));
         add(offlineLabel);
         numberOfPlayers = new JComboBox<>(new Integer[]{2, 3, 4});
-        numberOfPlayers.setBounds(307, 224, 64, 27);
+        numberOfPlayers.setBounds(588, 247, 85, 63);
         add(numberOfPlayers);
         startGameButton = new JButton("Start Game");
-        startGameButton.setBounds(386, 223, 112, 29);
+        startGameButton.setBounds(739, 253, 165, 41);
         startGameButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -52,22 +53,24 @@ public class WelcomePagePanel extends JPanel {
 
         // Online Section
         JLabel onlineLabel = new JLabel("ONLINE GAME MODE\n");
+        onlineLabel.setBounds(57, 330, 404, 42);
         onlineLabel.setForeground(new Color(255, 255, 255));
-        onlineLabel.setFont(new Font("Telugu MN", Font.BOLD | Font.ITALIC, 20));
-        onlineLabel.setBounds(23, 319, 256, 16);
+        onlineLabel.setFont(new Font("Telugu MN", Font.BOLD | Font.ITALIC, 30));
         add(onlineLabel);
 
         JTextField port = new JTextField("Port: ",10);
-        port.setBounds(105, 401, 130, 26);
+        port.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+        port.setBounds(211, 472, 250, 53);
         add(port);
 
         JTextField ip = new JTextField("IP: ",10);
-        ip.setBounds(297, 401, 130, 26);
+        ip.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+        ip.setBounds(211, 559, 250, 53);
         add(ip);
 
         
         hostGameButton = new JButton("Host Game");
-        hostGameButton.setBounds(508, 401, 113, 29);
+        hostGameButton.setBounds(587, 480, 158, 41);
         hostGameButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -81,7 +84,7 @@ public class WelcomePagePanel extends JPanel {
 
 
         joinGameButton = new JButton("Join Game");
-        joinGameButton.setBounds(508, 439, 107, 29);
+        joinGameButton.setBounds(587, 559, 163, 43);
         joinGameButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -93,34 +96,40 @@ public class WelcomePagePanel extends JPanel {
         add(joinGameButton);
         
         JLabel lblNewLabel = new JLabel("WELCOME TO KUALCHEMISTS!");
+        lblNewLabel.setBounds(257, 17, 821, 77);
         lblNewLabel.setForeground(new Color(255, 255, 255));
-        lblNewLabel.setFont(new Font("Telugu MN", Font.BOLD | Font.ITALIC, 35));
-        lblNewLabel.setBounds(84, 6, 568, 77);
+        lblNewLabel.setFont(new Font("Telugu MN", Font.BOLD | Font.ITALIC, 44));
         add(lblNewLabel);
         
         JLabel lblNewLabel_1 = new JLabel("Please select offline or online mode to start the game!\n");
-        lblNewLabel_1.setFont(new Font("Telugu MN", Font.BOLD | Font.ITALIC, 15));
+        lblNewLabel_1.setBounds(290, 106, 637, 27);
+        lblNewLabel_1.setFont(new Font("Telugu MN", Font.BOLD | Font.ITALIC, 23));
         lblNewLabel_1.setForeground(new Color(255, 255, 255));
-        lblNewLabel_1.setBounds(141, 114, 582, 16);
         add(lblNewLabel_1);
         
         JLabel lblNewLabel_2 = new JLabel("Please choose the number of players:");
+        lblNewLabel_2.setBounds(98, 251, 495, 39);
         lblNewLabel_2.setForeground(new Color(255, 255, 255));
-        lblNewLabel_2.setFont(new Font("Telugu MN", Font.BOLD | Font.ITALIC, 14));
-        lblNewLabel_2.setBounds(33, 229, 273, 16);
+        lblNewLabel_2.setFont(new Font("Telugu MN", Font.BOLD | Font.ITALIC, 23));
         add(lblNewLabel_2);
         
-        JLabel lblNewLabel_3 = new JLabel("Please enter your port and IP");
+        JLabel lblNewLabel_3 = new JLabel("To join a game, please enter the IP address and port number provided. \n");
+        lblNewLabel_3.setBounds(122, 413, 795, 27);
         lblNewLabel_3.setForeground(new Color(255, 255, 255));
-        lblNewLabel_3.setFont(new Font("Telugu MN", Font.BOLD | Font.ITALIC, 14));
-        lblNewLabel_3.setBounds(33, 370, 224, 16);
+        lblNewLabel_3.setFont(new Font("Telugu MN", Font.BOLD | Font.ITALIC, 20));
         add(lblNewLabel_3);
         
 		
 		JLabel lblNewLabel_4 = new JLabel("A game by FullMetal Alchemists");
+		lblNewLabel_4.setBounds(767, 663, 456, 53);
 		lblNewLabel_4.setForeground(new Color(255, 255, 255));
-		lblNewLabel_4.setFont(new Font("Luminari", Font.BOLD | Font.ITALIC, 19));
-		lblNewLabel_4.setBounds(400, 523, 306, 27);
+		lblNewLabel_4.setFont(new Font("Luminari", Font.BOLD | Font.ITALIC, 24));
 		add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("If you would like to host a game, please enter your port.");
+		lblNewLabel_5.setForeground(new Color(255, 255, 255));
+		lblNewLabel_5.setFont(new Font("Telugu MN", Font.BOLD | Font.ITALIC, 21));
+		lblNewLabel_5.setBounds(122, 375, 623, 39);
+		add(lblNewLabel_5);
     }
 }
