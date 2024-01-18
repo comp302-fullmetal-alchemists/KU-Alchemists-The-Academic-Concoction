d62a17cc7d1c6c618341034df60f42bf7469ff97
NeslisahCebeci <ncebeci19@ku.edu.tr>
2024-01-19 01:22:40 +0300

    authentication UI is modified

--- system/ui/panels/AuthenticationPanel.java
+++ system/ui/panels/AuthenticationPanel.java
@@ -24,9 +24,7 @@ public class AuthenticationPanel extends JPanel implements Observer {
 
 	private static final long serialVersionUID = 1L;
 	private JTextField username1;
-	private JTextField username2;
     private JComboBox token1;
-    private JComboBox token2;
 	private AuthenticationController authController;
 
 	/**
@@ -65,67 +63,37 @@ public class AuthenticationPanel extends JPanel implements Observer {
 
 		//created a text area for the player 1 username
     	JTextArea txtrPlayer1Username = new JTextArea();
-		txtrPlayer1Username.setText("Player 1 username:\n");
+		txtrPlayer1Username.setText("Player username:\n");
 		txtrPlayer1Username.setForeground(Color.WHITE);
 		txtrPlayer1Username.setFont(new Font("Microsoft Sans Serif", Font.BOLD | Font.ITALIC, 20));
 		txtrPlayer1Username.setBackground(new Color(58, 77, 108));
 		/*txtrPlayer1Username.setBounds(270, 212, 212, 32);*/
-		txtrPlayer1Username.setBounds(152, 176, 212, 32);
+		txtrPlayer1Username.setBounds(213, 200, 212, 32);
     	txtrPlayer1Username.setEditable(false);
 		add(txtrPlayer1Username);
-/* 
-		//created a text area for the player 2 username
-		JTextArea txtrPlayer2Username = new JTextArea();
-		txtrPlayer2Username.setText("Player 2 username:\n");
-		txtrPlayer2Username.setForeground(Color.WHITE);
-		txtrPlayer2Username.setFont(new Font("Microsoft Sans Serif", Font.BOLD | Font.ITALIC, 20));
-		txtrPlayer2Username.setBackground(new Color(58, 77, 108));
-		//txtrPlayer2Username.setBounds(905, 212, 141, 32);
-		txtrPlayer2Username.setBounds(687, 177, 221, 32);
-   		txtrPlayer2Username.setEditable(false);
-		add(txtrPlayer2Username);
-*/
+
 		//created a text area for the player 1 token
         JTextArea txtrPlayer1TokenChoose = new JTextArea();
-		txtrPlayer1TokenChoose.setText("Player 1 choose a token:\n");
+		txtrPlayer1TokenChoose.setText("Player choose a token:\n");
 		txtrPlayer1TokenChoose.setForeground(Color.WHITE);
 		txtrPlayer1TokenChoose.setFont(new Font("Microsoft Sans Serif", Font.BOLD | Font.ITALIC, 20));
 		txtrPlayer1TokenChoose.setBackground(new Color(58, 77, 108));
-		/*txtrPlayer1TokenChoose.setBounds(258, 397, 332, 32);*/
-		txtrPlayer1TokenChoose.setBounds(152, 307, 332, 32);
+		//txtrPlayer1TokenChoose.setBounds(152, 307, 332, 32);
+		txtrPlayer1TokenChoose.setBounds(600, 200, 332, 32);
     	txtrPlayer1TokenChoose.setEditable(false);
 		add(txtrPlayer1TokenChoose);
-/* 
-		//created a text area for the player 2 token
-		JTextArea txtrPlayer2TokenChoose = new JTextArea();
-		txtrPlayer2TokenChoose.setText("Player 2 choose a token:\n");
-		txtrPlayer2TokenChoose.setForeground(Color.WHITE);
-		txtrPlayer2TokenChoose.setFont(new Font("Microsoft Sans Serif", Font.BOLD | Font.ITALIC, 20));
-		txtrPlayer2TokenChoose.setBackground(new Color(58, 77, 108));
-		//txtrPlayer2TokenChoose.setBounds(880, 397, 296, 32);
-    	txtrPlayer2TokenChoose.setBounds(687, 307, 296, 32);
-    	txtrPlayer2TokenChoose.setEditable(false);
-		add(txtrPlayer2TokenChoose);
-*/
+
 		//created a text field for the player 1 to get username as input
 		username1 = new JTextField();
 		username1.setFont(new Font("Tahoma", Font.PLAIN, 21));
-		username1.setBounds(157, 229, 166, 53);
+		username1.setBounds(213, 253, 166, 53);
 		/*username1.setBounds(270, 276, 166, 53);*/
 		add(username1);
 		username1.setColumns(10);
-/* 
-		//created a text field for the player 2 to get username as input
-		username2 = new JTextField();
-		username2.setFont(new Font("Tahoma", Font.PLAIN, 21));
-		username2.setBounds(691, 229, 166, 53);
-		//username2.setBounds(905, 276, 166, 53);
-		username2.setColumns(10);
-		add(username2);
-*/
+
 		//created a combo box for the player 1 to choose a token
 		token1 = new JComboBox();
-		token1.setBounds(152, 357, 348, 268);
+		token1.setBounds(600, 253, 348, 268);
 		token1.setBackground(new Color(58, 77, 108));
 		token1.setModel(loadImages());
     	token1.addActionListener(new ActionListener() {
@@ -135,20 +103,7 @@ public class AuthenticationPanel extends JPanel implements Observer {
     	});
 		add(token1);
 		token1.setVisible(true);
-/* 
-		//created a combo box for the player 2 to choose a token
-		token2 = new JComboBox();
-		token2.setBounds(687, 357, 351, 268);
-		token2.setBackground(new Color(58, 77, 108));
-		token2.setModel(loadImages());
-		token2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				token2.getSelectedItem();
-			}
-		});
-		add(token2);
-		token2.setVisible(true);
-*/
+
 		//created a button for the players to login
 		JButton loginButton = new JButton("LOGIN");
 		loginButton.setFont(new Font("Tahoma", Font.PLAIN, 25));
@@ -165,7 +120,7 @@ public class AuthenticationPanel extends JPanel implements Observer {
 			}
 		});
 		/*loginButton.setBounds(1105, 747, 166, 46); */
-		loginButton.setBounds(978, 650, 166, 42);
+		loginButton.setBounds(978, 600, 166, 42);
 		add(loginButton);
 		
 		//created a text area for the game by message
@@ -175,7 +130,7 @@ public class AuthenticationPanel extends JPanel implements Observer {
 		txtrAGameBy.setFont(new Font("Luminari", Font.ITALIC, 18));
 		txtrAGameBy.setBackground(new Color(58, 77, 108));
 		/*txtrAGameBy.setBounds(1004, 803, 468, 32);*/
-		txtrAGameBy.setBounds(880, 695, 300, 32);
+		txtrAGameBy.setBounds(880, 660, 300, 32);
     	txtrAGameBy.setEditable(false);
 		add(txtrAGameBy);
 		//("/resources/token" + tokenIndex + ".png")
